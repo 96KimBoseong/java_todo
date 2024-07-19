36caffe2d763029572a60eac8f890244fe8f0113
kimboseong <njunju77@nate.com>
2024-07-19 14:18:38 +0900

    추가: 엔티티 + 베이스타임 엔티티

---
+++ src/test/java/com/todolist_java/TodoListJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todolist_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
