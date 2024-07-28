60a8ba33ea65b1bfe76d61189cf724ab73c65fd8
kimboseong <njunju77@nate.com>
2024-07-28 11:08:24 +0900

    추가: User엔티티 정의

--- src/main/java/com/todolist_java/domain/user/model/User.java
+++ src/main/java/com/todolist_java/domain/user/model/User.java
@@ -1,5 +1,6 @@
 package com.todolist_java.domain.user.model;
 
+import com.todolist_java.infra.BaseTimeEntity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -11,8 +12,21 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Getter
 @Table(name = "user")
-public class User {
+public class User extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name="nick_name")
+    private String nickName;
+
+    @Column(name = "user_name")
+    private String userName;
+
+    @Column(name="password")
+    private String password;
+
+    @Column(name = "role")
+    private UserRole role;
+
 }
